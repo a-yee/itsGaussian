117f00721e022d3b362449b0910660c2776021eb
Nez <yeekeiji@gmail.com>
2015-05-30 21:01:38 -0700

    complete function that can output matrix of emails in persons inbox location in the Metadata structure

--- rLoad.R
+++ rLoad.R
@@ -41,9 +41,63 @@ DF = Mth.delete(Metadata,1,2)
 # function will return row indices of emails where input recipient email string found
 # First specify a email string
 s = "tana.jones@enron.com"
-t <- sapply(s,function(s) which(apply(DF==s,1,any)));
+t <- matrix(sapply(s,function(s) which(apply(DF==s,1,any))),1)
 
 ## "t" is a vector of row indices where the function found the input email as a receiver of that
 # indexed email
 # uncomment to see vector
 # View(t)
+
+# following is an example of how it builds each row 
+s = "yao.apasu@enron.com"
+temp = matrix(sapply(s,function(s) which(apply(DF==s,1,any))),1)
+a <- rbind.fill.matrix(t,temp)
+View(a)
+
+
+# Need for use of rbind.fill.matrix
+library(plyr)
+
+# forces list of recipients to be a dataframe (only need to run once and reference often)
+uni_recip2=as.data.frame(uni_recip)
+
+# Load in row values of uni_recips you wants
+test_arr = read.csv("~/Documents/math191/project/itsGaussian/test_arr.csv", header = FALSE)
+
+# due to typing issues and other; define dataframe output as first row of output you want
+df = matrix(uni_recip2[test_arr[1,],])
+
+# define the number of rows you have as a double var
+k = dim(test_arr)[1]
+k = as.numeric(k)
+
+# For loop will output a 1-col matrix-class var holding all the names of the emails you want 
+for(i in 2:k){
+  temp = matrix(uni_recip2[test_arr[i,],])
+  df = rbind(df, temp)
+}
+
+
+# first iteration done b/c of init type errors trouble fixing
+s = df[1,]
+a = matrix(sapply(s,function(s) which(apply(DF==s,1,any))),1)
+
+# completes the matrix construction, needs further processing before export
+for(i in 2:k){
+s = df[i,]
+temp = matrix(sapply(s,function(s) which(apply(DF==s,1,any))),1)
+a <- rbind.fill.matrix(a,temp)
+}
+
+# Post processing making matrix ready for matlab
+# each column will correspond to an observation == unique person's list of inbox emails
+# irrelevant terms are NA atm --> transform to 0
+
+# replaces all na in matrix with a zero 
+a[is.na(a)] <- 0
+
+# takes transpose of matrix (want this so each column is an observation)
+a <- t(a)
+
+# write matrix to txt file 
+write.table(a, file="inbox.txt", row.names=FALSE, col.names=FALSE)
